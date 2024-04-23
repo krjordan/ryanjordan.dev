d2bb2fe0a0bb9a976d8f5c5355e9b35150004a7e
Ryan Jordan <ryjordan@gmail.com>
2024-04-23 11:30:18 -0500

    feat(app): add logo to header/footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import Image from 'next/image'
+import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
-      </div>
-    </footer>
-  );
-};
+	return (
+		<footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
+			<div className='container p-12 flex justify-between'>
+				<Image
+					src='/images/logo.png'
+					width={100}
+					height={100}
+					alt='logo'
+				/>
+				<p className='text-slate-600'>All rights reserved.</p>
+			</div>
+		</footer>
+	)
+}
 
-export default Footer;
+export default Footer
