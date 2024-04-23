6e18b30b2a36176027d171ff2d84a3e97036d966
Ryan Jordan <ryjordan@gmail.com>
2024-04-23 09:30:06 -0500

    fix(api): add email template

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -12,46 +12,7 @@ export async function POST(req, res) {
 			from: fromEmail,
 			to: [fromEmail, email],
 			subject: subject,
-			react: (
-				<>
-					<div
-						style={{
-							backgroundColor: '#f4f4f4',
-							padding: '20px',
-							fontFamily: 'Arial, sans-serif'
-						}}
-					>
-						<div
-							style={{
-								backgroundColor: '#fff',
-								padding: '20px',
-								borderRadius: '5px',
-								color: '#333'
-							}}
-						>
-							<h1>{subject}</h1>
-							<p style={{ color: '#555' }}>Thank you for contacting me!</p>
-							<p style={{ color: '#555' }}>
-								Your message has been received and I will get back to you soon.
-							</p>
-							<hr />
-							<p>
-								<strong>Message Details:</strong>
-							</p>
-							<blockquote
-								style={{
-									color: '#555',
-									borderLeft: '4px solid #ccc',
-									paddingLeft: '10px',
-									margin: '10px 0'
-								}}
-							>
-								{message}
-							</blockquote>
-						</div>
-					</div>
-				</>
-			)
+			react: EmailTemplate({ email, subject, message })
 		})
 		return NextResponse.json(data)
 	} catch (error) {
