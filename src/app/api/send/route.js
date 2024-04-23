a5e5924c3506e762966b5f82e48ee1886ccda115
Ryan Jordan <ryjordan@gmail.com>
2024-04-23 10:22:01 -0500

    testing

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -7,6 +7,7 @@ const fromEmail = process.env.RESEND_FROM_EMAIL
 export async function POST(req, res) {
 	const { email, subject, message } = await req.json()
 	console.log(email, subject, message)
+	console.log('apiKey', process.env.RESEND_API_KEY)
 	try {
 		const { data, error } = await resend.emails.send({
 			from: fromEmail,
